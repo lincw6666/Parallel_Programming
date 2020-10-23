a98748aae6fe05906be8819bc4ca7414d5666df0
lincw6666 <aunbdaunbd@gmail.com>
2020-10-23 14:00:03 +0800

    feat: replace rand() with simple linear random generator
    rand() is complicate and wastes time. After using simple generator, the execut-
    ion time becomes 0.27X, which is ~4X faster than using `rand()/RAND_MAX`.

--- HW2/pi.cpp
+++ HW2/pi.cpp
@@ -9,11 +9,6 @@ using namespace std;
 typedef unsigned long long ull;
 
 
-inline double get_rand() {
-    return (double)( (((ull)rand()<<31) | rand()) & 0x1FFFFFFFFFFFFF) / 0x1FFFFFFFFFFFFF;
-}
-
-
 int main(int argc, char **argv) {
     clock_t start;
     ull n_circle = 0; // # of darts in circle.
@@ -21,20 +16,20 @@ int main(int argc, char **argv) {
     double x, y, distance_squared;
     double pi_estimate = 0.0;
 
-    // Initialize random seeds.
-    srand(time(NULL));
-    
     start = clock();
-    for (ull toss = 0; toss < n_tosses; ++toss) {
-        x = get_rand();
-        y = get_rand();
+    for (ull toss = 0, seed = time(NULL); toss < n_tosses; ++toss) {
+        seed = ((seed * 1103515245U) + 12345U) & 0x7fffffff;
+        x = (double)seed / RAND_MAX;
+        seed = ((seed * 1103515245U) + 12345U) & 0x7fffffff;
+        y = (double)seed / RAND_MAX;
         distance_squared = x*x + y*y;
-        n_circle += (distance_squared <= 1);
+        if (distance_squared <= 1)
+            n_circle += 1;
     }
     printf("Time: %lf.\n", (clock()-start)/(double)CLOCKS_PER_SEC);
 
     pi_estimate = 4 * n_circle /((double)n_tosses);
-    cout << setprecision(9) << pi_estimate << endl;
+    printf("%.9lf\n", pi_estimate);
 
     return 0;
 }

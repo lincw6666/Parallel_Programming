67a755edc34b5ad883154f836ef26c8787e4a469
Cheng-Wei Lin <aunbdaunbd@gmail.com>
2020-10-25 02:22:13 +0000

    feat: [HW2-part1] ver2 of pi.cpp: good multi-threading
    In this version, time performance is better. However, when there are too many
    threads (more than 10240...), the estimation of pi is failed. It might be some
    problems with the random number generator, which becomes not random while using
    too many different seeds.

--- HW2/pi.cpp
+++ HW2/pi.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
-#include <ctime>
-#include <climits>
+#include <stdlib.h>
 #include <pthread.h>    // For Pthread
 using namespace std;
 
@@ -16,7 +15,7 @@ typedef struct thread_arg {
 } thread_arg_t;
 
 
-const ull g_seed = time(NULL);
+ull g_seed;
 ull g_n_circle = 0;
 pthread_mutex_t mutex_n_circle;
 
@@ -46,34 +45,49 @@ void *monte_carlo_pi_estimate(void *arg) {
 
 
 int main(int argc, char **argv) {
-    clock_t start;
     ull n_circle = 0; // # of darts in circle.
-    int n_thread = atoi(argv[1]);
+    int n_thread = atoi(argv[1]) - 1;
     ull n_tosses = atoi(argv[2]);    // # of tosses
     ull thread_tosses = n_tosses / (n_thread+1);
     ull seed;
     double x, y, distance_squared;
     double pi_estimate = 0.0;
-    pthread_t thread[n_thread];
-    thread_arg_t args[n_thread];
+    pthread_t *thread = NULL;
+    thread_arg_t *args = NULL;
 
-    start = clock();
+    if (n_thread < 1) {
+        seed = (ull)time(NULL);
+        for (int toss = 0; toss < n_tosses; ++toss) {
+            seed = ((seed * 1103515245U) + 12345U) & 0x7fffffff;
+            x = (double)seed / RAND_MAX;
+            seed = ((seed * 1103515245U) + 12345U) & 0x7fffffff;
+            y = (double)seed / RAND_MAX;
+            distance_squared = x*x + y*y;
+            if (distance_squared <= 1)
+                n_circle += 1;
+        }
+        pi_estimate = 4 * n_circle /((double)n_tosses);
+        printf("%.9lf\n", pi_estimate);
     
+        return 0;
+    }
+
+    // Allocate thread structure.
+    thread = (pthread_t *)malloc(n_thread * sizeof(pthread_t));
+    args = (thread_arg_t *)malloc(n_thread * sizeof(thread_arg_t));
     // Initialize mutex.
     pthread_mutex_init(&mutex_n_circle, NULL);
-    // Initialize thread attribute.
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-    for (int id = 0; id < n_thread; ++id) {
+    g_seed = (ull)time(NULL);
+    for (ull id = 0; id < n_thread; ++id) {
         args[id].thread_id = id;
         args[id].n_tosses = thread_tosses;
         pthread_create(&thread[id],
-                       &attr,
+                       NULL,
                        monte_carlo_pi_estimate,
                        (void *)&args[id]);
     }
+
     seed = g_seed + n_thread;
     for (int toss = n_thread*thread_tosses; toss < n_tosses; ++toss) {
         seed = ((seed * 1103515245U) + 12345U) & 0x7fffffff;
@@ -84,19 +98,18 @@ int main(int argc, char **argv) {
         if (distance_squared <= 1)
             n_circle += 1;
     }
-    // Destroy attribute.
-    pthread_attr_destroy(&attr);
 
     // Join threads.
     for (int id = 0; id < n_thread; ++id) {
         pthread_join(thread[id], NULL);
     }
 
+    free(thread);
+    free(args);
+
     // Destroy mutex.
     pthread_mutex_destroy(&mutex_n_circle);
     
-    printf("Time: %lf.\n", (clock()-start)/(double)CLOCKS_PER_SEC);
-
     pi_estimate = 4 * (g_n_circle+n_circle) /((double)n_tosses);
     printf("%.9lf\n", pi_estimate);
 

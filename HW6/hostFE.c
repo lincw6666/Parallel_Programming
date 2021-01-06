339c65194c39ffa917f869d7fb4ae650b6ac580e
Cheng-Wei Lin <aunbdaunbd@gmail.com>
2021-01-06 08:21:16 +0000

    feat: [HW6] ver.2: reduce if-else branches

--- HW6/hostFE.c
+++ HW6/hostFE.c
@@ -3,8 +3,6 @@
 #include "hostFE.h"
 #include "helper.h"
 
-int test = 0;
-
 void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
             float *inputImage, float *outputImage, cl_device_id *device,
             cl_context *context, cl_program *program)
@@ -12,6 +10,36 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
     cl_int status;
     int filterSize = filterWidth * filterWidth;
     int imageSize = imageHeight * imageWidth;
+    const int halfFilterWidth = filterWidth >> 1;
+
+    // ------------------------------------------------------------------------
+    // Split image into 5 parts: middle, upper, bottom, left, right
+    //
+    // For the middle part, we DON'T need to check whether the neighbors of a
+    // pixel exceed the boundary (imageHeight && imageWidth).
+    // For the rest parts, we SHOULD check whether the neighbors of a pixel
+    // exceed the boundary.
+    // ------------------------------------------------------------------------
+
+    // Set the parameters for each part.
+    size_t global_work_size[5][2] = {
+        {(size_t) imageWidth-filterWidth+1, (size_t) imageHeight-filterWidth+1},
+        {(size_t) imageWidth, (size_t) halfFilterWidth},
+        {(size_t) imageWidth, (size_t) halfFilterWidth},
+        {(size_t) halfFilterWidth, (size_t) imageHeight-filterWidth+1},
+        {(size_t) halfFilterWidth, (size_t) imageHeight-filterWidth+1}
+    };
+    int offset_x[5], offset_y[5];   // 0: middle, 1: upper, 2: bottom, 3: left,
+                                    // 4: right
+    offset_x[0] = halfFilterWidth, offset_y[0] = halfFilterWidth;
+    offset_x[1] = 0, offset_y[1] = 0;
+    offset_x[2] = 0, offset_y[2] = imageHeight - halfFilterWidth;
+    offset_x[3] = 0, offset_y[3] = halfFilterWidth;
+    offset_x[4] = imageWidth - halfFilterWidth, offset_y[4] = halfFilterWidth;
+
+    // ------------------------------------------------------------------------
+    // Create some OpenCL stuffs.
+    // ------------------------------------------------------------------------
 
     // Create a command queue.
     cl_command_queue command_queue = clCreateCommandQueue(*context, *device,
@@ -27,6 +55,10 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
     cl_mem filter_mem = clCreateBuffer(*context, CL_MEM_READ_ONLY,
                                        filterSize * sizeof(float),
                                        NULL, &status);
+    cl_mem offset_x_mem = clCreateBuffer(*context, CL_MEM_READ_ONLY,
+                                         sizeof(int), NULL, &status);
+    cl_mem offset_y_mem = clCreateBuffer(*context, CL_MEM_READ_ONLY,
+                                         sizeof(int), NULL, &status);
     cl_mem in_mem = clCreateBuffer(*context, CL_MEM_READ_ONLY,
                                    imageSize * sizeof(float),
                                    NULL, &status);
@@ -34,7 +66,14 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
                                     imageSize * sizeof(float),
                                     NULL, &status);
 
-    // Copy the lists A and B to their respective memory buffers
+    // Create the OpenCL kernel
+    cl_kernel kernel = clCreateKernel(*program, "convolution", &status);
+
+    // ------------------------------------------------------------------------
+    // Deal with the middle part.
+    // ------------------------------------------------------------------------
+
+    // Copy data to their respective memory buffers.
     status = clEnqueueWriteBuffer(command_queue, img_height_mem, CL_TRUE, 0,
                                   sizeof(int), &imageHeight, 0, NULL, NULL);
     printf("%d ", status);
@@ -48,13 +87,16 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
                                   filterSize * sizeof(float), filter,
                                   0, NULL, NULL);
     printf("%d ", status);
+    status = clEnqueueWriteBuffer(command_queue, offset_x_mem, CL_TRUE, 0,
+                                  sizeof(int), &offset_x[0], 0, NULL, NULL);
+    printf("%d ", status);
+    status = clEnqueueWriteBuffer(command_queue, offset_y_mem, CL_TRUE, 0,
+                                  sizeof(int), &offset_y[0], 0, NULL, NULL);
+    printf("%d ", status);
     status = clEnqueueWriteBuffer(command_queue, in_mem, CL_TRUE, 0, 
                                   imageSize * sizeof(float), inputImage,
                                   0, NULL, NULL);
     printf("%d ", status);
-
-    // Create the OpenCL kernel
-    cl_kernel kernel = clCreateKernel(*program, "convolution", &status);
 
     // Set the arguments of the kernel
     status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &img_height_mem);
@@ -65,16 +107,53 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
     printf("%d ", status);
     status = clSetKernelArg(kernel, 3, sizeof(cl_mem), (void *)&filter_mem);
     printf("%d ", status);
-    status = clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *)&in_mem);
+    status = clSetKernelArg(kernel, 4, sizeof(cl_mem), &offset_x_mem);
     printf("%d ", status);
-    status = clSetKernelArg(kernel, 5, sizeof(cl_mem), (void *)&out_mem);
+    status = clSetKernelArg(kernel, 5, sizeof(cl_mem), &offset_y_mem);
+    printf("%d ", status);
+    status = clSetKernelArg(kernel, 6, sizeof(cl_mem), (void *)&in_mem);
+    printf("%d ", status);
+    status = clSetKernelArg(kernel, 7, sizeof(cl_mem), (void *)&out_mem);
     printf("%d ", status);
 
-    // Execute the OpenCL kernel on the list
-    size_t global_work_size[2] = {(size_t) imageWidth, (size_t) imageHeight};
+    // Execute the OpenCL kernel
     status = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL,
-                                    global_work_size, 0, NULL, NULL, NULL);
+                                    global_work_size[0], 0, NULL, NULL, NULL);
     printf("%d ", status);
+
+    // ------------------------------------------------------------------------
+    // Now deal with upper, bottom, left and right parts.
+    // ------------------------------------------------------------------------
+    for (int i = 1; i < 5; ++i) {
+        status = clEnqueueWriteBuffer(command_queue, offset_x_mem, CL_TRUE, 0,
+                                      sizeof(int), &offset_x[i], 0, NULL, NULL);
+        printf("%d ", status);
+        status = clEnqueueWriteBuffer(command_queue, offset_y_mem, CL_TRUE, 0,
+                                      sizeof(int), &offset_y[i], 0, NULL, NULL);
+        printf("%d ", status);
+
+        // Set the arguments of the kernel
+        status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &img_height_mem);
+        printf("%d ", status);
+        status = clSetKernelArg(kernel, 1, sizeof(cl_mem), &img_width_mem);
+        printf("%d ", status);
+        status = clSetKernelArg(kernel, 2, sizeof(cl_mem), &filter_width_mem);
+        printf("%d ", status);
+        status = clSetKernelArg(kernel, 3, sizeof(cl_mem), (void *)&filter_mem);
+        printf("%d ", status);
+        status = clSetKernelArg(kernel, 4, sizeof(cl_mem), &offset_x_mem);
+        printf("%d ", status);
+        status = clSetKernelArg(kernel, 5, sizeof(cl_mem), &offset_y_mem);
+        printf("%d ", status);
+        status = clSetKernelArg(kernel, 6, sizeof(cl_mem), (void *)&in_mem);
+        printf("%d ", status);
+        status = clSetKernelArg(kernel, 7, sizeof(cl_mem), (void *)&out_mem);
+        printf("%d ", status);
+
+        status = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL,
+                                        global_work_size[i], 0, NULL, NULL, NULL);
+        printf("%d ", status);
+    }
 
     // Read the memory buffer C on the device to the local variable C
     status = clEnqueueReadBuffer(command_queue, out_mem, CL_TRUE, 0, 
@@ -90,6 +169,8 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
     status = clReleaseMemObject(img_width_mem);
     status = clReleaseMemObject(filter_width_mem);
     status = clReleaseMemObject(filter_mem);
+    status = clReleaseMemObject(offset_x_mem);
+    status = clReleaseMemObject(offset_y_mem);
     status = clReleaseMemObject(in_mem);
     status = clReleaseMemObject(out_mem);
     status = clReleaseCommandQueue(command_queue);
--- HW6/kernel.cl
+++ HW6/kernel.cl
@@ -1,24 +1,37 @@
 __kernel void convolution(
-    __global int *img_height,
-    __global int *img_width,
-    __global int *filter_width,
-    __global float *filter,
-    __global float *in_img,
-    __global float *out_img)
+    __constant int *img_height,
+    __constant int *img_width,
+    __constant int *filter_width,
+    __constant float *filter,
+    __constant int *offset_x,
+    __constant int *offset_y,
+    __read_only __global float *in_img,
+    __write_only __global float *out_img)
 {
-    const int x = get_global_id(0);
-    const int y = get_global_id(1);
+    const int x = get_global_id(0) + *offset_x;
+    const int y = get_global_id(1) + *offset_y;
 
     float tmp = 0;
     int a, b;
     const int ws = (*filter_width) >> 1;
 
-    for (int j = 0; j < *filter_width; ++j) {
-        for (int i = 0; i < *filter_width; ++i) {
-            a = x + i - ws;
-            b = y + j - ws;
-            if ((0 <= a && a < *img_width) && (0 <= b && b < *img_height)) {
+    if (*offset_x == *offset_y && *offset_x == ws) {
+        for (int j = 0; j < *filter_width; ++j) {
+            for (int i = 0; i < *filter_width; ++i) {
+                a = x + i - ws;
+                b = y + j - ws;
                 tmp += filter[j*(*filter_width) + i] * in_img[b*(*img_width) + a];
+            }
+        }
+    }
+    else {
+        for (int j = 0; j < *filter_width; ++j) {
+            for (int i = 0; i < *filter_width; ++i) {
+                a = x + i - ws;
+                b = y + j - ws;
+                if ((0 <= a && a < *img_width) && (0 <= b && b < *img_height)) {
+                    tmp += filter[j*(*filter_width) + i] * in_img[b*(*img_width) + a];
+                }
             }
         }
     }
